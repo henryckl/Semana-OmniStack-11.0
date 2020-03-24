d775cd63ff8bb2841ce994479fb6189f2c8deaa3
Henryck <henryck.leandro@gmail.com>
2020-03-24 13:35:11 -0300

    rota para criar ong adicionada

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,25 @@
 import { Router } from 'express';
+import { randomBytes } from 'crypto';
+
+import connection from './database/connection';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  const message = 'Hello, world';
-  return res.json({ message });
+routes.post('/ongs', async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+
+  const id = randomBytes(4).toString('HEX');
+
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf,
+  });
+
+  return response.json({ id });
 });
 
 export default routes;
