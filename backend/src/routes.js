c540ae455e6b652a1e0e3c771c348cdc6fae780f
Henryck <henryck.leandro@gmail.com>
2020-03-24 14:26:17 -0300

    IncidentController na pasta controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
-import connection from './database/connection';
 import OngController from './controllers/OngController';
+import IncidentController from './controllers/IncidentController';
 const routes = new Router();
 
 routes.get('/ongs', OngController.index);
-
 routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 export default routes;
