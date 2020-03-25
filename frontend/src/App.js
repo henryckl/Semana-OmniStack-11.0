7a527473380b89680ca382290343e889b3a7fc8d
Henryck <henryck.leandro@gmail.com>
2020-03-25 13:39:36 -0300

    logon page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import './global.css';
+import Logon from './pages/Logon';
 
 function App() {
-  return <h1>Hello, World</h1>;
+  return <Logon />;
 }
 
 export default App;
